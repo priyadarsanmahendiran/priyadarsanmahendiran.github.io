f4ed57121b114e451e157967d17af708732467d8
Priyadarsan Mahendiran <m.priyan2001@gmail.com>
2025-05-17 18:18:44 +0530

    Correct vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : "/priyadarsanmahendiran.github.io/",
+  base: "/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
